557554edfcbbd739e3e11a0545c0ec90ca5f5f35
Max Haviv <me03haviv@gmail.com>
2024-05-05 19:10:26 -0400

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="nav">
+      <img src='logo192.png' className="nav-image"></img>
+      <h3 className="nav-logo-text">ReactFacts</h3>
+
+      <h4 className="nav-title">React Course - Project 1</h4>
+
+    </nav>
+  )
+}
